7ec299e2252728c20da37d7c44abc67546d48690
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-04-11 19:28:27 -0500

    Implemented saving and retrieving saved data

--- IExpense/AddView.swift
+++ IExpense/AddView.swift
@@ -35,7 +35,14 @@ struct AddView: View
                 
                 TextField("Amount", value: $amount, format: .currency(code: "USD"));
             }
-            .navigationTitle("Add new expense");
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save")
+                {
+                    let item = ExpenseItem(name: name, type: type, amount: amount);
+                    expenses.items.append(item);
+                }
+            }
         }
     }
 }
